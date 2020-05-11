70baa3f361651256297f9a7dcabd79c27642a1f9
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-11 18:32:18 +0300

    Create mixins for the create, update, delete course handlers.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Course
 
-# Create your views here.
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        queryset = super(OwnerMixin, self).get_queryset()
+        return queryset.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/courses/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseEditMixin, DeleteView):
+    template_name = 'courses/manage/courses/delete.html'
+    success_url = reverse_lazy('manage_course_list')

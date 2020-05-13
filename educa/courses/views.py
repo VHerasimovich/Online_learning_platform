5ce1aba4c7d4fdd077d03d6c1dfef21a67016cc1
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-13 20:49:16 +0300

    Add course enroll function for students.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -11,6 +11,7 @@ from django.db.models import Count
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
+from students.forms import CourseEnrollForm
 
 
 class OwnerMixin(object):
@@ -177,3 +178,8 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context

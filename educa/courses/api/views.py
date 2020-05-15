540cda07a72ed2766b4415ca97bbdceeb33eb4da
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-15 16:10:32 +0300

    Add authentication and permissions check.

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
@@ -17,6 +19,9 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)   # connect student to the course

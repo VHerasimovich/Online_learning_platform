a7e38035b4b9198308c1c949b2e9a578cb3880de
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-15 15:56:22 +0300

    Fix error: didn't open course list view after student's registration.

--- educa/students/views.py
+++ educa/students/views.py
@@ -39,7 +39,7 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
 class StudentCourseListView(LoginRequiredMixin, ListView):
     model = Course
-    template_name = 'student/course/list.html'
+    template_name = 'students/course/list.html'
 
     def get_queryset(self):
         queryset = super(StudentCourseListView, self).get_queryset()

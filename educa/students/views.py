9f805c9bec8e923cd36fe22cd5d5e8460dab45e4
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-25 16:23:12 +0300

    Refactoring: change user registration template to the common style.

--- educa/students/views.py
+++ educa/students/views.py
@@ -58,7 +58,7 @@ def activate(request, uidb64, token):
 
 # class StudentRegistrationView(CreateView):
 #     template_name = 'students/student/registration.html'
-#     form_class = SignupForm
+#     form_class = UserCreationForm
 #     success_url = reverse_lazy('student_course_list')
 #
 #     def form_valid(self, form):
